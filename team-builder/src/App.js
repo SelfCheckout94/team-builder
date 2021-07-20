d40b2d1106ead483eb9bd63c49d35e616183818d
Christopher Boyle <cboyle824@gmail.com>
2021-07-20 09:48:35 -0400

    Some stuff works but doesn't add new member

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import Form from "./Form";
+import TeamMember from "./TeamMember";
 
 const initialFormValues = {
   name: "",
@@ -9,16 +10,29 @@ const initialFormValues = {
 };
 function App() {
   const [formValues, setFormValues] = useState(initialFormValues);
-  const [teamMember, setTeamMember] = useState([]);
+  const [teamMembers, setTeamMembers] = useState([]);
 
-  const updateForm = (inputName, inputValue) => {};
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue });
+  };
 
-  const submitForm = () => {};
-
+  const submitForm = () => {
+    const newTeamMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    };
+    if (!teamMembers.name || !teamMembers.email || !teamMembers.role) {
+      return;
+    }
+  };
   return (
     <div className="container">
       <h3>Submit your information to be added to the roster.</h3>
       <Form values={formValues} update={updateForm} submit={submitForm} />
+      {teamMembers.map((member) => {
+        return <TeamMember key="member.id" details={member} />;
+      })}
     </div>
   );
 }
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -3,23 +3,41 @@ import React, { useState } from "react";
 export default function Form(props) {
   const { values, update, submit } = props;
 
-  const onChange = (e) => {};
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    update(name, value);
+  };
 
-  const onSubmit = (e) => {};
+  const onSubmit = (e) => {
+    e.preventDefault();
+    submit();
+  };
 
   return (
     <form className="form container">
       <label>
         Name
-        <input />
+        <input
+          type="text"
+          name="name"
+          onChange={onChange}
+          value={values.name}
+          placeholder="Name"
+        />
       </label>
       <label>
         E-Mail
-        <input />
+        <input
+          type="email"
+          name="email"
+          onChange={onChange}
+          value={values.email}
+          placeholder="example@test.com"
+        />
       </label>
       <label>
         Role
-        <select name="role">
+        <select name="role" value={values.role} onChange={onChange}>
           <option value="">--Select Role--</option>
           <option value="Senior Dev">Senior Developer</option>
           <option value="Frontend">Front End Developer</option>
@@ -27,7 +45,9 @@ export default function Form(props) {
           <option value="Designer">Designer</option>
         </select>
         <div>
-          <button>Submit</button>
+          <button disabled={!values.name || !values.email || !values.role}>
+            Submit
+          </button>
         </div>
       </label>
     </form>
